654e430cba7a5019cb648248f11d58330ecb86d4
Hugo Vieira <hugo.vieira2002@hotmail.com>
2022-11-12 21:52:32 +0000

    adjust font sizes

--- scripts/toggleLight.js
+++ scripts/toggleLight.js
@@ -1,13 +1,25 @@
-const toggle = document.getElementById('toggleLight');
+// Variables
 const body = document.querySelector('body');
+const toggle = document.getElementById('toggleLight');
 
+// Function to switch the theme between dark and light
 toggle.addEventListener('click', function() {
-    this.classList.toggle('fa-moon');
-    if(this.classList.toggle('fa-sun')) {
-        console.log('Removed Class Light Mode');
-        body.classList.remove('light-mode');
-    } else {
-        console.log('Added Class Light Mode');
-        body.classList.add('light-mode');
+    // this.classList.toggle('fa-moon');
+    // if(this.classList.toggle('fa-sun')) {
+    //     body.classList.remove('light-mode');
+    //     localStorage.removeItem('themeSwitch');
+    // } else {
+    //     body.classList.add('light-mode');
+    //     localStorage.setItem('themeSwitch', 'light');
+    // }
+    if(body.classList.contains('light-theme') === false) {
+        this.classList.toggle('fa-moon');
+        if(this.classList.toggle('fa-sun')) {
+            body.classList.remove('light-mode');
+            localStorage.removeItem('themeSwitch');
+        } else {
+            body.classList.add('light-mode');
+            localStorage.setItem('themeSwitch', 'light');
+        }
     }
 })
